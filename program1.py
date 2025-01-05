fafda1d6800963990086d3014c0db4483fd1c593
Khushal4408x <yadavkhushal25@gmail.com>
2025-01-05 03:23:06 -0500

    .

---
+++ program1.py
@@ -0,0 +1,10 @@
+a=10
+b="2"
+x=int (b) +1#typecasting
+print(x)
+print(a/3)#quotient in float
+print (a//3) #quotient in int
+#assignment operator and comparision opearators are same
+#!!!!!IMPORTANT
+print(not (False))##prints true
+print(not (True))
