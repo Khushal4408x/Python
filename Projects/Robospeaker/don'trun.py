a09d44f805ae602d0fdd59aa3c610332a3b4a593
Khushal4408x <yadavkhushal25@gmail.com>
2025-01-06 04:43:21 +0000

    Update don'trun.py

--- Projects/Robospeaker/don'trun.py
+++ Projects/Robospeaker/don'trun.py
@@ -4,5 +4,5 @@ if __name__== '__main__':
     print("Welcome to robospeaker 1.1. created by Khushal")
     while (True):
        
-        command=f"espeak '{'teri maki choot'}'"# install espeak in kali by 
+        command=f"espeak '{'Infinite loop'}'"# install espeak in kali by 
         os.system(command)
