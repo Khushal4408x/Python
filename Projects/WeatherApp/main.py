486a99628d5e852b0abec77e37a37edfc737854d
Khushal4408x <yadavkhushal25@gmail.com>
2025-01-07 12:03:37 -0500

    .

--- Projects/WeatherApp/main.py
+++ Projects/WeatherApp/main.py
@@ -1,1 +1,33 @@
 import requests
+
+def get_weather(city, api_key):
+    # OpenWeatherMap API endpoint
+    url = f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&aqi=no"
+    #http://api.weatherapi.com/v1/current.json?key={api_key}}&q={city}&aqi=no
+    # Make a GET request to the API
+    response = requests.get(url)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        # Extract relevant information
+        city_name = data['location']['name']
+        region = data['location']['region']
+        country= data['location']['country']
+        tempra
+        weather_description = data['weather'][0]['description']
+        
+        # Print the weather information
+        print(f"Weather in {city_name}:")
+        print(f"Temperature: {temperature}°C")
+        print(f"Description: {weather_description.capitalize()}")
+    else:
+        print("City not found. Please check the name and try again.")
+
+def main():
+    api_key = "bcecc816e68a4050aa8163505250501"  
+    city = input("Enter the city name: ")
+    get_weather(city, api_key)
+
+if __name__ == "__main__":
+    main()
---
+++ collegeworkshop/stings.py
@@ -0,0 +1,6 @@
+name='khushal'
+print(name)
+print (name[0:4])
+#[start:end index:step size] end index is not inclusive; start index is inclusive
+print (name.capitalize())
+print (name.isnumeric())
