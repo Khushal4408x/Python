f21e1ffa95cbe0764c341aa2c913fb178a3700a3
Khushal4408x <yadavkhushal25@gmail.com>
2025-01-08 00:38:26 -0500

    simple interest

---
+++ collegeworkshop/simpleintrest.py
@@ -0,0 +1,6 @@
+p=int ( input('enter the principle price'))
+r=int (input('enter the rate % '))
+t=int (input ('enter the time in years'))
+print("simple intrest = " , (p*r*t)/100)
+si=(p*r*t)/100
+print ("final price to pay " ,si+p )
