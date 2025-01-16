d39fb5fd9a9182da4d682a11216c4a9fa704b04f
Khushal4408x <yadavkhushal25@gmail.com>
2025-01-16 02:01:34 -0500

    .

---
+++ collegeworkshop/filehandling/ascitochar.py
@@ -0,0 +1,9 @@
+f=open("/home/kali/Documents/coding/Pythonbg/collegeworkshop/filehandling/ascinum.txt","rt")
+r=open("/home/kali/Documents/coding/Pythonbg/collegeworkshop/filehandling/ascinum.txt","a")
+content=f.read()
+print(type())
+contlst=content.split(" ")
+print(contlst)
+contlstconv=[]
+i=0
+while(i)
