d82211dccb4ae208242d0015671a03caf7d72d7e
Khushal4408x <yadavkhushal25@gmail.com>
2025-01-17 11:23:22 -0500

    .

--- collegeworkshop/filehandling/ascitochar.py
+++ collegeworkshop/filehandling/ascitochar.py
@@ -1,9 +1,14 @@
 f=open("/home/kali/Documents/coding/Pythonbg/collegeworkshop/filehandling/ascinum.txt","rt")
 r=open("/home/kali/Documents/coding/Pythonbg/collegeworkshop/filehandling/ascinum.txt","a")
 content=f.read()
-print(type())
+print(type(content))
 contlst=content.split(" ")
 print(contlst)
-contlstconv=[]
+print(len(contlst))
+finalist=[]
 i=0
-while(i)
+while(i<len(finalist)):
+    x=int(contlst[i])
+    finalist.append(x)
+    i=i+1
+print("int list ",finalist)    
--- exceptionhandling.py
+++ exceptionhandling.py
@@ -3,3 +3,6 @@ try:
     print(a+3)
 except Exception as e:# Exception as e assigns the error to e and we can print the error
     print ("some error occured",e)  # its not necessary to assign and print the exception to a variable  
+finally:
+    print("This part will execute even when the exception occured")
+        
