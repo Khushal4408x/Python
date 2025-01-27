b5c0efd89e15cf8194e32c954470c87620bd3d30
Khushal4408x <yadavkhushal25@gmail.com>
2025-01-27 11:44:19 +0530

    .

--- collegeworkshop/nestedsqprint.py
+++ collegeworkshop/nestedsqprint.py
@@ -1,7 +1,11 @@
-n=int(input("enter the num: "))
-i=1
-for i in range(n):
-    j=1
-    for j in range(n):
-        print(i,end=" ")
-    print()    
+# n=int(input("enter the num: "))
+# i=1
+# for i in range(n):
+#     j=1
+#     for j in range(n):
+#         print(i,end=" ")
+#     print()    
+for i  in range (3):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
