4bde5b106cea021734773d8f659b1d0d5043a961
Khushal4408x <yadavkhushal25@gmail.com>
2025-06-15 13:31:25 -0400

    Tkinter_basix

---
+++ Tkinter_GUI/basix.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root =Tk()
+root.title('Login form')
+#root.iconbitmap('heart.ico')
+#root.maxsize(500,500)
+root.geometry("350x500")
+#root.configure(background="Blue")
+root.configure(background="#40E0E6EA")
+root.mainloop()
